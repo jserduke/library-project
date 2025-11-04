c9358fb6b8dc60b77919614202eebf637504fb35
pespitro-danica <dpespitro@horizon.csueastbay.edu>
2025-11-04 15:57:42 -0800

    testing eclipse to git import
    - Danica

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Our library project!");
+		System.out.println("Testing my Eclipse to Git import - Danica (:");
 	}
 }
