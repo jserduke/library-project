1cc405c3dcd7d90c171148eede7ffc19e8a35976
Thanh Nguyen <tnguyen626@horizon.csueastbay.edu>
2025-11-04 16:16:48 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Our library project!");
 		System.out.println("Testing my Eclipse to Git import - Danica (:");
+		System.out.println("Testing this is lynn");
 	}
 }
