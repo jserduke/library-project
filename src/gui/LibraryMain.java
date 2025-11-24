cd63816d64537f1a45d992b678a27547f64f2190
Jason Serduke <jasonserduke@jasons-air-5.lan>
2025-11-24 13:44:45 -0800

    Just whittling down the differences

--- src/gui/LibraryMain.java
+++ src/gui/LibraryMain.java
@@ -5,6 +5,7 @@ import javax.swing.SwingUtilities;
 
 public class LibraryMain {
     public static void main(String[] args) {
+    	/*
     	ArrayList<String> info = new ArrayList<String>();
     	info.add("Local Library");
     	
@@ -26,12 +27,13 @@ public class LibraryMain {
 		info.add("4");
 		info.add("0");
         SwingUtilities.invokeLater(() -> new WelcomeDashboardFrame(null, null, info).setVisible(true));
+        */
     	/*
     	User found = LibraryData.findUser("member", "member123");
     	(new MemberPortalFrame(found)).setVisible(true);
     	*/
     	// User found = LibraryData.findUser("admin", "admin123");
-    	/*
+    	// /*
     	ArrayList<String> info = new ArrayList<String>();
     	info.add("BOOK");
     	info.add("1");
@@ -59,7 +61,7 @@ public class LibraryMain {
     	info.add("~120");
     	info.add("5");
     	info.add("5");
-    	(new AdminPortalFrame(info)).setVisible(true);
-    	*/
+    	(new AdminPortalFrame(null, null, info)).setVisible(true);
+    	// */
     }
 }
