66a13a2565be6050e22bd1817ba5428b8da1a03f
pespitro-danica <dpespitro@horizon.csueastbay.edu>
2025-11-30 04:01:42 -0800

    deleting old comments

--- src/gui/MemberAccountDialog.java
+++ src/gui/MemberAccountDialog.java
@@ -3,33 +3,13 @@ package gui;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
-import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 import client.*;
-import message.*;
-import message.Status;
 
 public class MemberAccountDialog extends JDialog {
     private static final long serialVersionUID = 1L;
-//    private final ResponseHandler responseHandler;
-//	private final ObjectOutputStream requestWriter;
-//	private HoldsPanel holdsPanel;
 
-//	public MemberAccountDialog(Frame owner, int memberId){
-//        super(owner, "My Holds & Fees", true);
-//        setSize(900, 560);
-//        setLocationRelativeTo(owner);
-//
-//        JTabbedPane tabs = new JTabbedPane();
-//        tabs.addTab("My Holds", new HoldsPanel(memberId));
-//        tabs.addTab("My Late Fees", new LateFeesPanel(memberId));
-//
-//        setLayout(new BorderLayout());
-//        add(tabs, BorderLayout.CENTER);
-//    }
-    
     public MemberAccountDialog(Frame owner,
     						   ObjectOutputStream requestWriter,
     						   ResponseHandler responseHandler,
@@ -47,8 +27,6 @@ public class MemberAccountDialog extends JDialog {
         
         tabs.addTab("My Holds", holdsPanel);
         tabs.addTab("My Late Fees", new LateFeesPanel(requestWriter, responseHandler));
-
-//      setLayout(new BorderLayout());
         add(tabs, BorderLayout.CENTER);
         
         addWindowListener(new java.awt.event.WindowAdapter() {
@@ -58,22 +36,5 @@ public class MemberAccountDialog extends JDialog {
         		responseHandler.setActiveHoldsPanel(null);
         	}
         });
-//        add(tabs);
-        
-//        responseHandler.setActiveHoldsPanel(holdsPanel);
-        
-//        ArrayList<String> info = new ArrayList<>();
-//        Message msg = new Message(0, message.Type.REQUEST, -1, message.Action.GET_HOLDS, Status.PENDING, info);
-//        
-//        responseHandler.setRequestIdExpected(msg.getId());
-//        responseHandler.setOldFrame(this);
-//        
-//        try {
-//        	requestWriter.writeObject(msg);
-//        } catch (IOException e) {
-//        	e.printStackTrace();
-//        }
-//    responseHandler.setRequestIdExpected(-1);
-//    responseHandler.setOldWindow(this);
     }
 }
