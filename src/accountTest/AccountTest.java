2402ecd002ce21e53727af8a508fd5b3af291984
pespitro-danica <dpespitro@horizon.csueastbay.edu>
2025-12-01 03:34:12 -0800

    Update AccountTest.java

--- src/accountTest/AccountTest.java
+++ src/accountTest/AccountTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class AccountTest {
+public class AccountTest {
 	
 	private Account account;
 	private Date birthday;
